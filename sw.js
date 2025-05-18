cbbf7ce6a5e91a9e740118c3eeb34538a7f4f5a4
tasuketekure <ariruka1415@gmail.com>
2025-05-18 20:37:45 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-
-self.addEventListener("install", function(event) {
-  console.log("Service Worker installed");
-});
-
-self.addEventListener("fetch", function(event) {
-  // Optionally cache resources here
-});
+...
