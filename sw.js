a072f03bfab13fa04479d47530ba2b89f886f275
tasuketekure <ariruka1415@gmail.com>
2025-05-20 21:16:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-self.addEventListener('push', event => {
+
+self.addEventListener('push', function(event) {
   const data = event.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: data.icon
-  });
+  const title = data.title || '通知';
+  const options = {
+    body: data.body || '通知が届きました。',
+    icon: 'stamp.png'
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
 });
